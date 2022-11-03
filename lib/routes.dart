15decb20b72af2558085149d890578d0631d011f
Khanh_do_huy <rithamto@gmail.com>
2022-11-03 02:53:01 +0000

    ONLINE COMMIT

--- lib/routes.dart
+++ lib/routes.dart
@@ -2,7 +2,6 @@ import 'package:flutter/widgets.dart';
 
 import 'package:coolmate/screens/splash/splash_screen.dart';
 
-
 final Map<String, WidgetBuilder> routes = {
   SplashScreen.routeName: (context) => SplashScreen(),
 };
